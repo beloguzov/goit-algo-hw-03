61fafa9d556fe2201b84ff9bb0e3cc84aa0d1cf7
beloguzov <143844879+beloguzov@users.noreply.github.com>
2024-03-12 00:11:14 +0200

    Update hw-03-04.py

--- hw-03-04.py
+++ hw-03-04.py
@@ -30,18 +30,18 @@ def get_upcoming_birthdays(users):
     for user in prepared_users:  
         birthday_this_year = user["birthday"].replace(year=today.year)  
 
-    if birthday_this_year < today:  
-        birthday_this_year = birthday_this_year.replace(year=today.year + 1)  
+        if birthday_this_year < today:  
+            birthday_this_year = birthday_this_year.replace(year=today.year + 1)  
 
-    if 0 <= (birthday_this_year - today).days <= days:  
-        if birthday_this_year.weekday() >= 5: 
-            birthday_this_year = find_next_weekday(birthday_this_year, 0) 
+        if 0 <= (birthday_this_year - today).days <= days:  
+            if birthday_this_year.weekday() >= 5: 
+                birthday_this_year = find_next_weekday(birthday_this_year, 0) 
 
-        congratulation_date_str = birthday_this_year.strftime('%Y.%m.%d') 
-        upcoming_birthdays.append({  
-            "name": user["name"],
-            "congratulation_date": congratulation_date_str
-        })
+            congratulation_date_str = birthday_this_year.strftime('%Y.%m.%d') 
+            upcoming_birthdays.append({  
+                "name": user["name"],
+                "congratulation_date": congratulation_date_str
+            })
     
     return upcoming_birthdays
 
