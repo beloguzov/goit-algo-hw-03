66ea77a19f7e750cd3df3a13135648008e235d42
beloguzov <143844879+beloguzov@users.noreply.github.com>
2024-03-11 23:40:24 +0200

    Update hw-03-04.py

--- hw-03-04.py
+++ hw-03-04.py
@@ -8,12 +8,22 @@ users = [
     {"name": "Jane Smith1", "birthday": "1990.03.10"},
 ]
 
-def prepare_users(users):
+def get_upcoming_birthdays(users):
     prepared_users = []  
     for user in users: 
         try:
-            birthday = datetime.strptime(user['birthday'], '%Y.%m.%d').date()  # Парсимо дату народження
-            prepared_users.append({"name": user['name'], 'birthday': birthday})  # Додаємо користувача з підготовленою датою народження
+            birthday = datetime.strptime(user['birthday'], '%Y.%m.%d').date()  
+            prepared_users.append({"name": user['name'], 'birthday': birthday})  
         except ValueError:
-            print(f'Некоректна дата народження для користувача {user["name"]}')  # Виводимо повідомлення про помилку
-    return prepared_users
+            print(f'Некоректна дата народження для користувача {user["name"]}')  
+
+
+
+
+
+    
+    return upcoming_birthdays
+
+
+upcoming_birthdays = get_upcoming_birthdays(users)
+print("Список привітань на цьому тижні:", upcoming_birthdays)
