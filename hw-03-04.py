8772fe0f2b8125e19d7b1780efe8b6b321b933e5
beloguzov <143844879+beloguzov@users.noreply.github.com>
2024-03-11 23:52:49 +0200

    Update hw-03-04.py

--- hw-03-04.py
+++ hw-03-04.py
@@ -9,6 +9,13 @@ users = [
 ]
 
 def get_upcoming_birthdays(users):
+
+    def find_next_weekday(d, weekday: int):  
+        days_ahead = weekday - d.weekday()  
+        if days_ahead <= 0:  
+            days_ahead += 7  
+        return d + timedelta(days=days_ahead)  
+    
     prepared_users = []  
     for user in users: 
         try:
@@ -17,10 +24,24 @@ def get_upcoming_birthdays(users):
         except ValueError:
             print(f'Некоректна дата народження для користувача {user["name"]}')  
 
+    days = 7
+    today = datetime.today().date()
+    upcoming_birthdays = []  
+    for user in prepared_users:  
+        birthday_this_year = user["birthday"].replace(year=today.year)  
 
+    if birthday_this_year < today:  
+        birthday_this_year = birthday_this_year.replace(year=today.year + 1)  
 
+    if 0 <= (birthday_this_year - today).days <= days:  
+        if birthday_this_year.weekday() >= 5: 
+            birthday_this_year = find_next_weekday(birthday_this_year, 0) 
 
-
+        congratulation_date_str = birthday_this_year.strftime('%Y.%m.%d') 
+        upcoming_birthdays.append({  
+            "name": user["name"],
+            "congratulation_date": congratulation_date_str
+        })
     
     return upcoming_birthdays
 
